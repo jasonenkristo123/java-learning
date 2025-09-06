97d851506c0b55e9ca8a8ba3991916af772b9593
jasonenkristo123 <jasonenkristo@gmail.com>
2025-09-06 23:36:47 +0700

    latihan multiclass panggil

---
+++ src/Contoh/Class1.java
@@ -0,0 +1,10 @@
+package Contoh;
+
+public class Class1 {
+    public void mesin() {
+        System.out.println("Mesin nya kenceng banget");
+    }
+    public void kenceng(int maxKecepatan) {
+        System.out.println("kecepatan mobil ini adalah: " + maxKecepatan + "kilo per jam");
+    }
+}
---
+++ src/Contoh/Panggil1.java
@@ -0,0 +1,9 @@
+package Contoh;
+
+class Panggil1 {
+    public static void main(String[] args) {
+        Class1 Obj = new Class1();
+        Obj.mesin();
+        Obj.kenceng(399);
+    }
+}
