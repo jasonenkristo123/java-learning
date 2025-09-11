1af44d581f063cfb39e3063c126ea36eba355cd8
jasonenkristo123 <jasonenkristo@gmail.com>
2025-09-11 22:23:10 +0700

    latihan static and prrivate class

--- src/OOP/InnerClasss.java
+++ src/OOP/InnerClasss.java
@@ -22,3 +22,39 @@ class printInfo2 {
         System.out.println(outer.x + inner.y);
     }
 }
+
+// private inner class
+// Unlike a "regular" class, an inner class can be private or protected. 
+// If you don't want outside objects to access the inner class, declare the class as private:
+
+class outerClass1 {
+    int x = 10;
+
+    private class innerClass1 {
+        int y = 100;
+    }
+}
+
+class printInfo3 {
+    public static void main(String[] args) {
+        outerClass1 myOuter = new outerClass1();
+        outerClass1.innerClass1 myInner = myOuter.new innerClass1(); // akan eror karena classnya di private
+    }
+}
+
+// static inner class
+// An inner class can also be static, which means that you can access it without creating an object of the outer class:
+class outerClass2 {
+    int x = 100;
+
+    static class innerClass2 {
+        int y = 200;
+    }
+}
+
+class printInfo4 {
+    public static void main(String[] args) {
+        outerClass2.innerClass2 myInner = new outerClass2.innerClass2();
+        System.out.println(myInner.y);
+    }
+}
