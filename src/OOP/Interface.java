f1cce024dcca8d0481409d6f2822643b68e789c0
jasonenkristo123 <jasonenkristo@gmail.com>
2025-09-12 23:59:13 +0700

    belajar interface

---
+++ src/OOP/Interface.java
@@ -0,0 +1,16 @@
+package OOP;
+public class Interface {
+}
+// Another way to achieve abstraction in Java, is with interfaces.
+// An interface is a completely "abstract class" that is used to group related methods with empty bodies:
+
+// example
+interface Animal {
+  public void animalSound(); // interface method (does not have a body)
+  public void run(); // interface method (does not have a body)
+}
+/*
+ * To access the interface methods, 
+ * the interface must be "implemented" (kinda like inherited) by another class with the implements keyword (instead of extends). 
+ * The body of the interface method is provided by the "implement" class:
+ */
