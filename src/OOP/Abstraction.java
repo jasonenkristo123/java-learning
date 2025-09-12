4ee981a05bb29c77f7f9f0ba266bb7c353652906
jasonenkristo123 <jasonenkristo@gmail.com>
2025-09-12 23:56:16 +0700

    abstraction + inheritance

--- src/OOP/Abstraction.java
+++ src/OOP/Abstraction.java
@@ -1,5 +1,40 @@
 package OOP;
-
 public class Abstraction {
+}
+/*
+Data abstraction is the process of hiding certain details and showing only essential information to the user.
+Abstraction can be achieved with either abstract classes or interfaces (which you will learn more about in the next chapter).
+The abstract keyword is a non-access modifier, used for classes and methods:
+Abstract class: is a restricted class that cannot be used to create objects (to access it, it must be inherited from another class).
+Abstract method: can only be used in an abstract class, and it does not have a body. The body is provided by the subclass (inherited from).
+An abstract class can have both abstract and regular methods:
+ */
 
+//  abstract class Animal {
+//   public abstract void animalSound();
+//   public void sleep() {
+//     System.out.println("Zzz");
+//   }
+// From the example above, it is not possible to create an object of the Animal class:
+// To access the abstract class, it must be inherited from another class.
+// Let's convert the Animal class we used in the Polymorphism chapter to an abstract class:
+abstract class Animal {
+    public abstract void animalSound(); // abstract method
+    public void sleep() { // reguler method
+        System.out.println("zzz");
+    }
+}
+
+class Pig extends Animal {
+    public void animalSound() {
+        System.out.println("oink oink oink ahg");
+    }
+}
+
+class printInfo6 {
+    public static void main(String[] args) {
+    Pig obj = new Pig();
+    obj.animalSound();
+    obj.sleep();
+    }
 }
