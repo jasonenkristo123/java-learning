e46fa4b04d52a027650b4fe0927221f24af759a5
jasonenkristo123 <jasonenkristo@gmail.com>
2025-09-13 00:35:22 +0700

    multiple interfaces

--- src/OOP/Interface.java
+++ src/OOP/Interface.java
@@ -24,7 +24,7 @@ class Dogs implements Animall {
     public void animalSound() { // body of animalSound provided here
         System.out.println("hahahahah goinkkkk");
     }
-     // membuat ulang method yang sudah ada di parent class/interface
+    @Override // membuat ulang method yang sudah ada di parent class/interface
     public void sleep() {
         System.out.println("brbbbbb zzzzz");
     }
@@ -36,4 +36,43 @@ class printInfo7 {
         Obj.sleep();
     }
 }
- 
+
+/*
+Like abstract classes, interfaces cannot be used to create objects
+(in the example above, it is not possible to create an "Animal" object in the MyMainClass)
+Interface methods do not have a body - the body is provided by the "implement" class
+On implementation of an interface, you must override all of its methods
+Interface methods are by default abstract and public
+Interface attributes are by default public, static and final
+An interface cannot contain a constructor (as it cannot be used to create objects)
+Why And When To Use Interfaces?
+1) To achieve security - hide certain details and only show the important details of an object (interface).
+2  Java does not support "multiple inheritance" (a class can only inherit from one superclass). 
+However, it can be achieved with interfaces, because the class can implement multiple interfaces. 
+Note: To implement multiple interfaces, separate them with a comma (see example below).
+ */
+
+ interface firstInterface {
+    public void myMethod1();
+ }
+
+ interface secondInterface {
+    public void myMethod2();
+ }
+
+ class demoClass implements firstInterface, secondInterface {
+    public void myMethod1() {
+        System.out.println("halo sayang");
+    }
+    public void myMethod2() {
+        System.out.println("apa sayang");
+    }
+ }
+
+ class printInfo8 {
+    public static void main(String[] args) {
+        demoClass obj = new demoClass();
+        obj.myMethod1();
+        obj.myMethod2();
+    }
+ }
