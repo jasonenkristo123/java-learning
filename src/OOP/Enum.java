069cba64c9cc7f251311a5558e31089d83d76904
jasonenkristo123 <jasonenkristo@gmail.com>
2025-09-13 21:55:17 +0700

    belajar loop di enum

--- src/OOP/Enum.java
+++ src/OOP/Enum.java
@@ -1,7 +1,6 @@
 package OOP;
 
 // An enum is a special "class" that represents a group of constants (unchangeable variables, like final variables).
-// To create an enum, use the enum keyword (instead of class or interface), and separate the constants with a comma. 
 // Note that they should be in uppercase letters:
 enum Level {
     LOW,   // kayak array 
@@ -28,5 +27,54 @@ class Enum1 {
     public static void main(String[] args) {
         Level obj = Level.veryhard;
         System.out.println(obj);
+    }
+}
+
+// enum di switch 
+
+enum Level1 {
+    easy,
+    medium,
+    hard,
+    impossible
+}
+
+class enum1 {
+    public static void main(String[] args) {
+        Level1 level = Level1.impossible;
+
+        switch(level) {
+            case easy:
+                System.out.println("gampang banget");
+            break;
+            case medium:
+                System.out.println("mayan susah si");
+            break;
+            case hard:
+                System.out.println("susah ni dahan");
+            break;
+            case impossible:
+                System.out.println("gabakal bisa si ini");
+            break;
+        }
+    }
+}
+
+// Loop di enum
+enum nama {
+    jason,
+    arfa,
+    brian,
+    hanif,
+    habibi,
+    qhalbi,
+    rafi
+}
+
+class nama1 {
+    public static void main(String[] args) {
+        for (nama objNama : nama.values()) {
+        System.out.println(objNama);
+        }
     }
 }
