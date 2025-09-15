73c489af5ea84fe3317907417201cb0e2a2fb4c9
jasonenkristo123 <jasonenkristo@gmail.com>
2025-09-15 21:06:11 +0700

    buat task managernya

--- src/Project/ATesting.java
+++ src/Project/ATesting.java
@@ -18,6 +18,26 @@ public class ATesting {
             System.out.println("tidak ditemukan");
         }
 
-        System.out.println("nilai rata rata adalah " + manager.getAverageGrade());
+        System.out.println("nilai rata rata mahasiswa adalah " + manager.getAverageGrade());
+    }
+}
+
+class testing1 {
+    public static void main(String[] args) {
+        Task task = new Task("Belajar java");
+        Task task1 = new Task("Kerja math");
+
+        System.out.println("daftar tugas awal");
+        System.out.println(task);
+        System.out.println(task1);
+
+        task.markCompleted();
+        task1.markCompleted();
+
+        System.out.println("\nSetelah menandai satu selesai:");
+        System.out.println(task);
+        System.out.println(task1);
+
+        
     }
 }
--- src/Project/Task.java
+++ src/Project/Task.java
@@ -1,5 +1,30 @@
 package Project;
 
 public class Task {
+    private String description; // isi tugasnya
+    private boolean completed; // udah selesai atau belum
 
+    public Task(String description) {
+        if (description == null || description.isEmpty()) {
+            throw new IllegalArgumentException("tugas belum ada");
+        }
+        this.description = description.trim();
+        this.completed = false; // default kalau belum selesai
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void markCompleted() {
+        this.completed = true;
+    }
+    @Override
+    public String toString() {
+        return (completed ? "[X]" : "[ ] ") + description;
+    }
 }
