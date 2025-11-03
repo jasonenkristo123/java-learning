bef74c5068ad59a99d4342a93e585aab6835d169
jasonenkristo123 <jasonenkristo@gmail.com>
2025-11-03 15:36:10 +0700

    method laprak

--- src/Playground/setres.java
+++ src/Playground/setres.java
@@ -250,3 +250,17 @@ class setres5 {
         return angka % 2 == 0;
     }
 }
+
+class testing {
+public static void main(String[] args) { 
+int hasil; 
+int bilangan = 5; 
+
+hasil = faktorial(bilangan); 
+System.out.println("Nilai dari "+ bilangan + "! adalah " 
++ hasil); } 
+
+private static int faktorial(int bil){ if (bil==1) 
+return 1; 
+else return (bil * faktorial(bil-1)); } 
+} 
